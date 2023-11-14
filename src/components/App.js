a4f3f282df734d9dfcfb4a0f505c893813db3d7c
Jerico <jericodcosta95375788@gmail.com>
2023-11-14 12:51:36 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
-
+import axios from "axios";
 import React from "react";
+import { useState,useEffect } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+  const [data,setData] = useState({fetching:false , data : []});
+  
+  useEffect(()=>{
+    fetchData();
+  },[])
+   
+  function fetchData(){
+     setData({fetching:true , data:[]});
+     axios.get("https://dummyjson.com/products").then((data)=>{
+        setData({fetching:false , data: data.data});
+        console.log(data.data.products);
+     })
+  }
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      {
+        data.fetching ? <h1>Loading...</h1> : <h1>Data Fetched from API</h1>
+      }
+      {
+        data !== null ? <pre>{
+            JSON.stringify(data.data)
+          }</pre> : ""
+      }
     </div>
   )
 }
